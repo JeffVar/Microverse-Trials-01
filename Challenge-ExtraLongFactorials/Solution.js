8fb083afd9da70963a539269f8e7a5f47da971d8
Jefferson <jeff-4-1@hotmail.com>
2022-11-02 12:38:19 -0500

    Added the Hello World message

---
+++ Challenge-ExtraLongFactorials/Solution.js
@@ -0,0 +1,41 @@
+'use strict';
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', function(inputStdin) {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', function() {
+    inputString = inputString.split('\n');
+
+    main();
+});
+
+function readLine() {
+    return inputString[currentLine++];
+}
+
+/*
+ * Complete the 'extraLongFactorials' function below.
+ *
+ * The function accepts INTEGER n as parameter.
+ */
+
+function extraLongFactorials(n) {
+    // Write your code here
+    let fact = 1;
+    for (let i = 1; i < (n+1); i++){
+        fact *= i;
+    }
+}
+
+function main() {
+    const n = parseInt(readLine().trim(), 10);
+
+    extraLongFactorials(n);
+}
